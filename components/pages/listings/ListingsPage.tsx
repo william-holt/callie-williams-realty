5b03c131d403a846f51b99b37c28b94aeed74a1e
williamholt <williamholt0004@gmail.com>
2024-07-23 13:53:54 -0400

    Added filters to the Listings Page

--- components/pages/listings/ListingsPage.tsx
+++ components/pages/listings/ListingsPage.tsx
@@ -1,16 +1,35 @@
+'use client';
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
+import { Button } from '@/components/shared/Button'
 import { CustomPortableText } from '@/components/shared/CustomPortableText'
 
 export function ListingsPage({data}: any) {
+  const [currentFilter, setCurrentFilter] = useState('Residential')
+  const [viewableListings, setViewableListings] = useState<any[]>([])
+
+  useEffect(() => {
+    if (data?.length) {
+      const activeListings = data.filter( (listing: any) => listing.tags.includes(currentFilter));
+
+      setViewableListings(activeListings)
+    }
+  }, [data, currentFilter])
 
   return (
     <div className="space-y-20 flex flex-col">
       <div>Listings </div>
+      <div>Current Filter: {currentFilter}</div>
+      <div className="flex flex-row justify-around items-center">
+        <Button color="paper" size="md" variant="solid" onClick={() => setCurrentFilter('Residential')}>Residential</Button>
+        <Button color="paper" size="md" variant="solid" onClick={() => setCurrentFilter('Development')}>Development</Button>
+        <Button color="paper" size="md" variant="solid" onClick={() => setCurrentFilter('Commercial')}>Commercial</Button>
+      </div>
 
-      {data?.length && (
+      {viewableListings?.length > 0 ? (
         <div className="flex flex-row flex-wrap justify-start items-start">
-          {data.map((listing: any, index: number) => {
+          {viewableListings.map((listing: any, index: number) => {
             return (
               <div key={'listing' + index} className="flex flex-col items-start justify-center p-2 m-2 border-black border-2">
                 <div>name: {listing.name}</div>
@@ -48,6 +67,8 @@ export function ListingsPage({data}: any) {
             )
           })}
         </div>
+      ) : (
+        <div>There are no Listings that match this filter</div>
       )}
     </div>
   )
