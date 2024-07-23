beb1e50b566cb5c95e145fe09a162929bbc06ba4
williamholt <williamholt0004@gmail.com>
2024-07-23 11:01:48 -0400

    Updated the Property Slug page to use a preview page as well.

--- app/(personal)/properties/[slug]/page.tsx
+++ app/(personal)/properties/[slug]/page.tsx
@@ -1,17 +1,16 @@
 import type { Metadata, ResolvingMetadata } from 'next'
 import dynamic from 'next/dynamic'
 import { draftMode } from 'next/headers'
-import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import { toPlainText } from 'next-sanity'
 
-import { CustomPortableText } from '@/components/shared/CustomPortableText'
+import SinglePropertyPage from '@/components/pages/properties/SinglePropertyPage'
 import { urlForOpenGraphImage } from '@/sanity/lib/utils'
 import { generateStaticSlugs } from '@/sanity/loader/generateStaticSlugs'
-import { loadProject, loadProperty } from '@/sanity/loader/loadQuery'
-// const ProjectPreview = dynamic(
-//   () => import('@/components/pages/project/ProjectPreview'),
-// )
+import { loadProperty } from '@/sanity/loader/loadQuery'
+const SinglePropertyPagePreview = dynamic(
+  () => import('@/components/pages/properties/SinglePropertyPreviewPage'),
+)
 
 type Props = {
   params: { slug: string }
@@ -44,50 +43,15 @@ export function generateStaticParams() {
 export default async function PropertySlugRoute({ params }: Props) {
   const initial = await loadProperty(params.slug)
 
-  // if (draftMode().isEnabled) {
-  //   return <ProjectPreview params={params} initial={initial} />
-  // }
+  if (draftMode().isEnabled) {
+    return <SinglePropertyPagePreview initial={initial} />
+  }
 
   if (!initial.data) {
     notFound()
   }
 
   const {data: listing} = initial;
-
-  // return <ProjectPage data={initial.data} />
-  return (
-    <div>
-      <div>
-        Individual Project Page
-      </div>
-      <div className="flex flex-col items-start justify-center p-2 m-2 border-black border-2">
-        <div>name: {listing.name}</div>
-        <div>status: {listing.status}</div>
-        <div>description: {listing.description}</div>
-        <div>location: {listing.location}</div>
-        <div>price: {listing.price}</div>
-        <div>square footage: {listing.squareFootage}</div>
-        <div>bedrooms: {listing.bedroomCount}</div>
-        <div>bathrooms: {listing.bathroomCount}</div>
-        <div>construction date: {listing.constructionDate}</div>
-        <CustomPortableText
-          paragraphClasses="text-md md:text-xl"
-          value={listing.features}
-        />
-        {listing.testimonials?.length > 0 && (
-          <div>
-            {listing.testimonials.map((testimonial: any, testimonialIndex: number) => {
-              return (
-                <div key={'testimonial' + testimonialIndex}>
-                  <div>{testimonial.name}</div>
-                  <div>{testimonial.review}</div>
-                  <div>{testimonial.date}</div>
-                </div>
-              )
-            })}
-          </div>
-        )}
-      </div>
-    </div>
-  )
+  
+  return <SinglePropertyPage initial={initial} listing={listing} />
 }
---
+++ components/pages/properties/SinglePropertyPage.tsx
@@ -0,0 +1,42 @@
+import { CustomPortableText } from '@/components/shared/CustomPortableText'
+
+export function PropertiesPage({data: listing}: any) {
+
+  return (
+    <div className="space-y-20 flex flex-col">
+      <div>
+        Individual Project Page
+      </div>
+      <div className="flex flex-col items-start justify-center p-2 m-2 border-black border-2">
+        <div>name: {listing.name}</div>
+        <div>status: {listing.status}</div>
+        <div>description: {listing.description}</div>
+        <div>location: {listing.location}</div>
+        <div>price: {listing.price}</div>
+        <div>square footage: {listing.squareFootage}</div>
+        <div>bedrooms: {listing.bedroomCount}</div>
+        <div>bathrooms: {listing.bathroomCount}</div>
+        <div>construction date: {listing.constructionDate}</div>
+        <CustomPortableText
+          paragraphClasses="text-md md:text-xl"
+          value={listing.features}
+        />
+        {listing.testimonials?.length > 0 && (
+          <div>
+            {listing.testimonials.map((testimonial: any, testimonialIndex: number) => {
+              return (
+                <div key={'testimonial' + testimonialIndex}>
+                  <div>{testimonial.name}</div>
+                  <div>{testimonial.review}</div>
+                  <div>{testimonial.date}</div>
+                </div>
+              )
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default PropertiesPage
