fd2be7dcc7569261326bdb9b9f0c5dda93c9f4ca
Austin Isinghood <austin@austinisinghood.com>
2024-07-25 19:29:27 -0600

    Basically finishing the layout of the featured properties component.

--- components/pages/home/HomePageFeaturedProperties.tsx
+++ components/pages/home/HomePageFeaturedProperties.tsx
@@ -28,15 +28,65 @@ export function HomePageFeaturedProperties(props: propertyProps) {
               <Arrows />
             </div>
           </div>
-          <div className="w-full min-h-[300px] bg-secondary-light p-4 rounded-border-lg shadow-lg">
-            <code>property</code>
+          <div className="w-full min-h-[300px] flex flex-col items-center justify-center bg-secondary-light p-4 rounded-border-lg shadow-lg lg:flex-row lg:items-stretch">
+            {/* Thumbnail */}
+            <div className="w-full p-1 rounded-border-sm shadow-sm lg:w-1/2 lg:pr-2">
+              <div className="w-full h-full min-h-[300px] rounded-border-sm shadow-sm lg:mb-0 bg-secondary" />
+            </div>
+            {/* Features */}
+            <div className="w-full rounded-border-sm shadow-sm lg:w-1/2">
+              {/* Details */}
+              <div className="w-full p-1">
+                <div className="w-full flex flex-col items-start justify-start p-4 rounded-border-sm shadow-sm bg-paper-light sm:flex-row">
+                  <ul className="w-full flex flex-col gap-y-2 p-1 sm:w-1/2">
+                    <li>
+                      <code>detail 1</code>
+                    </li>
+                    <li>
+                      <code>detail 2</code>
+                    </li>
+                    <li>
+                      <code>detail 3</code>
+                    </li>
+                  </ul>
+                  <ul className="w-full flex flex-col gap-y-2 p-1 sm:w-1/2">
+                    <li>
+                      <code>detail 4</code>
+                    </li>
+                    <li>
+                      <code>detail 5</code>
+                    </li>
+                    <li>
+                      <code>detail 6</code>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              {/* Gallery */}
+              <div className="w-full flex flex-col items-center justify-center flex-wrap md:flex-row">
+                <div className="w-full h-24 p-1 md:h-48 md:w-1/2 lg:h-72">
+                  <div className="w-full h-full rounded-border-sm shadow-sm  bg-secondary" />
+                </div>
+                <div className="w-full flex flex-row flex-wrap md:w-1/2">
+                  <div className="w-1/3 h-24 p-1 md:w-full lg:h-36">
+                    <div className="w-full h-full rounded-border-sm shadow-sm  bg-secondary" />
+                  </div>
+                  <div className="w-1/3 h-24 p-1 md:w-1/2 lg:h-36">
+                    <div className="w-full h-full rounded-border-sm shadow-sm  bg-secondary" />
+                  </div>
+                  <div className="w-1/3 h-24 p-1 md:w-1/2 lg:h-36">
+                    <div className="w-full h-full rounded-border-sm shadow-sm bg-secondary" />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="w-full flex flex-row items-center justify-center py-4 md:hidden">
             <Arrows />
           </div>
         </div>
       </section>
-      <div className="h-[250px] bg-primary-light -mt-[200px]" />
+      <div className="h-[250px] bg-paper-light -mt-[300px] md:-mt-[200px]" />
     </>
   )
 }
