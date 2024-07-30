7e6352eecbdd1b0425a4c063323dfe31c600be23
Austin Isinghood <austin@austinisinghood.com>
2024-07-29 19:59:23 -0600

    Fixing background on navbar.

--- components/global/Navbar/NavbarLayout.tsx
+++ components/global/Navbar/NavbarLayout.tsx
@@ -31,38 +31,40 @@ export default function Navbar(props: NavbarProps) {
   }
 
   return (
-    <nav className="sticky top-0 z-10 w-full max-w-screen-2xl h-[100px] flex flex-row items-center justify-center flex-wrap mx-auto px-4 backdrop-blur lg:justify-between">
-      <Menu menuItems={menuItems} isOpen={isOpen} setIsOpen={setIsOpen} />
-      <div className={twMerge(`w-fit order-1 lg:hidden`)}>
-        <Hamburger
-          toggled={isOpen}
-          toggle={toggleMenu}
-          duration={0.75}
-          size={25}
-          distance="sm"
-          color="#FFFFFF"
-        />
-      </div>
-      <div className="relative z-10 w-fit order-2 lg:order-1 xl:w-1/3 xl:justify-end">
-        <Image
-          className="w-fit max-w-[250px] md:max-w-[350px] lg:max-w-[450px] xl:hidden"
-          src="/navbar-logo-mobile-v1.png"
-          alt="Callie Williams logo"
-          width={500}
-          height={250}
-        />
-        <Image
-          className="hidden w-fit max-w-[450px] xl:block"
-          src="/navbar-logo-v1.png"
-          alt="Callie Williams logo"
-          width={500}
-          height={250}
-        />
-      </div>
-      <div className="hidden w-fit items-center justify-end order-3 xl:w-1/3 xl:flex">
-        <Button color="paper" size="md" variant="solid">
-          Contact Us
-        </Button>
+    <nav className="sticky top-0 z-10 w-full h-[100px] bg-gradient-to-b from-ink-dark to-transparent">
+      <div className="w-full max-w-screen-2xl flex flex-row items-center justify-center flex-wrap mx-auto px-4 lg:justify-between">
+        <Menu menuItems={menuItems} isOpen={isOpen} setIsOpen={setIsOpen} />
+        <div className={twMerge(`w-fit order-1 lg:hidden`)}>
+          <Hamburger
+            toggled={isOpen}
+            toggle={toggleMenu}
+            duration={0.75}
+            size={25}
+            distance="sm"
+            color="#FFFFFF"
+          />
+        </div>
+        <div className="relative z-10 w-fit order-2 lg:order-1 xl:w-1/3 xl:justify-end">
+          <Image
+            className="w-fit max-w-[250px] md:max-w-[350px] lg:max-w-[450px] xl:hidden"
+            src="/navbar-logo-mobile-v1.png"
+            alt="Callie Williams logo"
+            width={500}
+            height={250}
+          />
+          <Image
+            className="hidden w-fit max-w-[450px] xl:block"
+            src="/navbar-logo-v1.png"
+            alt="Callie Williams logo"
+            width={500}
+            height={250}
+          />
+        </div>
+        <div className="hidden w-fit items-center justify-end order-3 xl:w-1/3 xl:flex">
+          <Button color="paper" size="md" variant="solid">
+            Contact Us
+          </Button>
+        </div>
       </div>
     </nav>
   )
