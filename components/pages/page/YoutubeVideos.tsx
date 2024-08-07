25e99c5b6ceb4f25dbb220fe112a8470f963c737
williamholt <williamholt0004@gmail.com>
2024-08-07 14:30:18 -0400

    Updated the youtube video to rely on the Youtube Playlist.

--- components/pages/page/YoutubeVideos.tsx
+++ components/pages/page/YoutubeVideos.tsx
@@ -14,22 +14,14 @@ export function YoutubeVideos({videos}: any) {
   }, [videos])
 
   return (
-    <section className="w-full bg-secondary-dark p-24">
-      {videoList.length && (
-        <>
-          {videoList.map((video: any, index: number) => (
-            <div key={index}>
-              <iframe
-                src={video}
-                height={400}
-                width={500}
-                allowFullScreen
-
-              />
-            </div>
-          ))}
-        </>
-      )}
+    <section className="w-full bg-secondary-dark p-24 flex flex-row justify-center items-center">
+      <iframe width="560"
+              height="315"
+              src="https://www.youtube.com/embed/videoseries?si=PvkXFU6q2doUpf5V&amp;list=PL5UW3FdDyuvEBF9_C97eUUgV3Vm3Jrla6"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen></iframe>
       {/*<HomePageFeaturedProperties*/}
       {/*  title={'Featured Listings Being Shown Now'}*/}
       {/*  properties={featuredPropertyData}*/}
